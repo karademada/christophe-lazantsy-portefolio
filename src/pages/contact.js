4d7a937a47d36b7741dc689fb9393e01cda03287
christophe Lazantsy <christophe.lazantsy@gmaill.com>
2021-04-28 12:52:39 +0200

    add contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,9 +7,35 @@ const ContactPage = () => {
     <div>
       <Layout>
         <Seo title="Contactez moi"/>
-        <h1>Contact!</h1>
+        <h1>Contactez pour un projet.</h1>
+        <form 
+          name="contact v1"
+          method="post"
+          data-netlify="true"
+          onSubmit="submit"  
+        >
+          <input type="hidden" name="form-name" value="contact v1" />
+          <div>
+            <label htmlFor="nom">Nom</label><br />
+            <input id="nom" type="type" name="nom" />
+          </div>
+          <div>
+            <label htmlFor="prenom">Prenom</label><br />
+            <input id="prenom" type="type" name="prenom" />
+          </div>
+          <div>
+            <label htmlFor="email">Email</label><br />
+            <input id="email" type="type" name="email" />
+          </div>
+          <div>
+            <label htmlFor="email">Commentaire</label><br />
+            <textarea id="email" type="type" name="comments"></textarea>
+          </div>
+          <br/>
+          <button>Envoyez les informations</button>
+        </form>
         <p>
-          Contact me on
+          Contactez moi sur 
           <a
             href="https://twitter.com/christophelazan"
             target="_blank"
