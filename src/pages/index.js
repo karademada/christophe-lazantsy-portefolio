0df074d591c86fb3957d3ffa8d4a215decf01b9a
christophe Lazantsy <christophe.lazantsy@gmaill.com>
2021-04-28 11:59:01 +0200

    Add static image

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import { Link } from "gatsby"
 import React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import { StaticImage } from "gatsby-plugin-image"
 
 const IndexPage = () => (
   <div>
@@ -11,6 +12,15 @@ const IndexPage = () => (
       <h3>
         I'm Salahdin Lazantsy a fullstack senior developer, living in Créteil.
       </h3>
+      {/* <img src="./images/panoramique-creteil.png" alt="Vue levé de soleil panoramique de créteil, depuis le parc de créteil" /> */}
+      <StaticImage
+        src="../images/panoramique-creteil.png"
+        width={750}
+        quality={95}
+        formats={["AUTO", "WEBP", "AVIF"]}
+        alt="Vue levé de soleil panoramique de créteil, depuis le parc de créteil"
+        style={{ marginBottom: `1.45rem` }}
+      />
       <p>
         Need a developer ? <Link to="/contact">Contact me!</Link>
       </p>
