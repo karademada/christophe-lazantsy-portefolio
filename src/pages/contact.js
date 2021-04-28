52ea3c5df31dc15b2703378c99a012f6922de355
christophe Lazantsy <christophe.lazantsy@gmaill.com>
2021-04-28 19:59:06 +0200

    add honeypot

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,36 +6,48 @@ const ContactPage = () => {
   return (
     <div>
       <Layout>
-        <Seo title="Contactez moi"/>
+        <Seo title="Contactez moi" />
         <h1>Contactez pour un projet.</h1>
-        <form 
+        <form
           name="contact v1"
           method="post"
           data-netlify="true"
-          onSubmit="submit"  
+          data-netlify-honeypot="bot-field"
+          onSubmit="submit"
         >
           <input type="hidden" name="form-name" value="contact v1" />
+          <div hidden>
+            <input name="bot-field" />
+          </div>
           <div>
-            <label htmlFor="nom">Nom</label><br />
+            <label htmlFor="nom">Nom</label>
+            <br />
             <input id="nom" type="type" name="nom" />
           </div>
           <div>
-            <label htmlFor="prenom">Prenom</label><br />
+            <label htmlFor="prenom">Prenom</label>
+            <br />
             <input id="prenom" type="type" name="prenom" />
           </div>
           <div>
-            <label htmlFor="email">Email</label><br />
+            <label htmlFor="email">Email</label>
+            <br />
             <input id="email" type="type" name="email" />
           </div>
           <div>
-            <label htmlFor="commentaire">Commentaire</label><br />
-            <textarea id="commentaire" type="type" name="commentaire"></textarea>
+            <label htmlFor="commentaire">Commentaire</label>
+            <br />
+            <textarea
+              id="commentaire"
+              type="type"
+              name="commentaire"
+            ></textarea>
           </div>
-          <br/>
+          <br />
           <button>Envoyez les informations</button>
         </form>
         <p>
-          Contactez moi sur 
+          Contactez moi sur
           <a
             href="https://twitter.com/christophelazan"
             target="_blank"
