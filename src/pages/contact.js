a1fae626feb22cacea39c698546acc5789286f3e
christophe Lazantsy <christophe.lazantsy@gmaill.com>
2021-05-08 19:27:15 +0200

    fix build

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -20,7 +20,7 @@ const ContactPage = () => {
           method="post"
           data-netlify="true"
           data-netlify-honeypot="bot-field"
-          onSubmit={handleSubmit(submit)} // eslint-disable-line
+          onSubmit={handleSubmit('submit')} // eslint-disable-line
         >
           <input type="hidden" name="form-name" value="contact v1" />
           <div hidden>
